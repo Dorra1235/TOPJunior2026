990b249e8c6d74a62a484c2f188a98d6b292c2ae
Dorra1235 <natsuotodoroki69@gmail.com>
2026-02-12 21:33:38 +0100

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fi first 
+#define se second
+using namespace std;
+void solve(){
+    return;
+}
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    int t = 1;
+    while (t--)solve();
+    return 0;
+}
